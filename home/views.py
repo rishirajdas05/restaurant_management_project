822a17afed8a297ae3dd69842cda5b647e05f662
rishirajdas05 <154807904+rishirajdas05@users.noreply.github.com>
2025-08-16 00:08:53 +0530

    32368

--- home/views.py
+++ home/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.conf import settings
 
 def index(request):
     return render(request, "home/index.html")
 def about(request):
     return render(request, "about.html")
+def index(request):
+    context = {
+        "restaurant_name": settings.RESTAURANT_NAME
+    }
+    return render(request, "index.html", context)
 
-    
 # Create your views here.
