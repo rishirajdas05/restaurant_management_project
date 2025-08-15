c4d0ab194c9f2a7d9a15297d173541decbd26a97
rishirajdas05 <154807904+rishirajdas05@users.noreply.github.com>
2025-08-15 23:54:52 +0530

    32369

--- home/views.py
+++ home/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "home/index.html")
+def about(request):
+    return render(request, "about.html")
 # Create your views here.
