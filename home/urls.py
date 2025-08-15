446e69f39875e035d97e1610bff3ca328d41476f
rishirajdas05 <154807904+rishirajdas05@users.noreply.github.com>
2025-08-15 23:55:53 +0530

    32369

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = [ path("", index, name="home")]
+urlpatterns = [ 
+    path("", index, name="home"),
+    path("about/",about,name="about"),
+    
+
+
+]
