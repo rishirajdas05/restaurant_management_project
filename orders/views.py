7657ac06c4abaede73d1f03f1c045f871ae72871
rishirajdas05 <154807904+rishirajdas05@users.noreply.github.com>
2025-08-26 18:52:26 +0530

    35558

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def menu_list(request):
+    # Hardcoded menu list for now
+    menu_items = [
+        {"id": 1, "name": "Margherita Pizza", "price": 250},
+        {"id": 2, "name": "Veggie Burger", "price": 180},
+        {"id": 3, "name": "Pasta Alfredo", "price": 220},
+        {"id": 4, "name": "Cold Coffee", "price": 120},
+    ]
+    return render(request, "menu_list.html", {"menu_items": menu_items})
