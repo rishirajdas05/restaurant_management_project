baecf7fb06f27af1571079c10e67282ddb74d7f3
rishirajdas05 <154807904+rishirajdas05@users.noreply.github.com>
2025-08-28 23:27:33 +0530

    39529

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path("", index, name="home"),
     path("about/",about,name="about"),
     path("contact/", views.contact, name="contact"),
+    path("reservations/", views.reservations, name="reservations"),
+
 
 
 ]
