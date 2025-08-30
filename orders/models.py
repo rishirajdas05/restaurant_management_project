ea67506735fa4c30832edb4a5e9edd5c1c8142ec
rishirajdas05 <154807904+rishirajdas05@users.noreply.github.com>
2025-08-30 23:10:56 +0530

    40514

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Feedback(models.Model):
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # First 40 chars as a preview
+        return (self.comment[:40] + "…") if len(self.comment) > 40 else self.comment
