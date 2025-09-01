e80e2c7518cec8dfe4847468f2eebaf08f570123
rishirajdas05 <154807904+rishirajdas05@users.noreply.github.com>
2025-09-02 02:29:54 +0530

    40955

--- account/models.py
+++ account/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    phone = models.CharField(max_length=15, blank=True, null=True)
+
+    # Since User already has name and email fields:
+    #   - name = user.first_name + user.last_name
+    #   - email = user.email
+
+    def __str__(self):
+        return self.user.username
+
+
+# Auto-create / save profile whenever a User is created or updated
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        instance.profile.save()
