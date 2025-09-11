7241f079bf12adeb1657cfbd8c22319d55a7fe4a
rishirajdas05 <154807904+rishirajdas05@users.noreply.github.com>
2025-09-12 01:54:49 +0530

    42361

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ContactMessage
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "email", "created_at")
+    ordering = ("-created_at",)
