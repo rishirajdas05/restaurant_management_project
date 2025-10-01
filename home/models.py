fb3a5aaae386dc4bdddb373d030cc072197ce5fc
rishirajdas05 <154807904+rishirajdas05@users.noreply.github.com>
2025-10-01 17:29:35 +0530

    42363

--- home/models.py
+++ home/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import Decimal
 
-class ContactMessage(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
+class MenuItem(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField(blank=True)  # optional
+    price = models.DecimalField(
+        max_digits=8, decimal_places=2,
+        validators=[MinValueValidator(Decimal("0.00"))]
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.name} ({self.email})"
+        return self.name
